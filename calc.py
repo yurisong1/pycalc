63bf0a983469a19b47c268c98bb58a279e8cc798
Preshith Kulkarni <preshith.kulkarni@monash.edu>
2019-12-11 14:57:02 +1100

    Use fancy-schmancy iterable unpacking

--- calc.py
+++ calc.py
@@ -1,10 +1,9 @@
 def compute(expression):
-    values = expression.split(' ')
-    num0 = int(values[0])
-    operator = values[1]
-    num1 = int(values[2])
+    num0, operator, num1 = expression.split(' ')
     if operator == '+':
         return num0 + num1
+    elif operator == '-':
+        return num0 - num1
     else:
         print('unknown operator!')
-        return 0
+        return None
